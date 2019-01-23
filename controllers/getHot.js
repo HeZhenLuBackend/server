344d584fee3d1d17ecf3f27150115924931c87b1
hxh <hanxinhu521@163.com>
2019-01-23 22:45:24 +0800

    更新查询方式

--- controllers/getHot.js
+++ controllers/getHot.js
@@ -1,10 +1,8 @@
 const mysql = require('../middleware/mysql');
 
 module.exports = async (ctx) =>{
-    let result = await mysql('article').select('title',{id:'aid'},{coverPicture:'cover'},'script','type','date');
-     result = result.filter((item, index)=>{
-         if(index>=4) return 0;
-    })
+    let result = await mysql('article').select('title',{id:'aid'},{coverPicture:'cover'},'script','type','date').limit(4);
+
     ctx.body ={
         data:result
     }
