2f8025a8c6f9f8f4b0ec293ec1bdb6df090b17f4
UsagiHao <506887402@qq.com>
2019-01-26 14:05:51 +0800

    添加passage/class路由

--- config.js
+++ config.js
@@ -15,7 +15,8 @@ const CONF = {
             /^\/api\/v1\/user\/register/,
             /^\/api\/v1\/passage\/hot/,
             /^\/api\/v1\/passage\/recommend/,
-            /^\/api\/v1\/passage\/addHot/
+            /^\/api\/v1\/passage\/addHot/,
+            /^\/api\/v1\/passage\/class/
         ]
     },
     url: {
