0a23f8b3e59dc3aeec88e5a04d4358f22d25664f
apple <875172749@qq.com>
2019-01-27 14:51:20 +0800

    add comment

--- config.js
+++ config.js
@@ -18,7 +18,8 @@ const CONF = {
             /^\/api\/v1\/passage\/addHot/,
             /^\/api\/v1\/passage\/class/,
             /^\/api\/v1\/passage\/latest/,
-            /^\/api\/v1\/comment\/latest/
+            /^\/api\/v1\/comment\/latest/,
+            /^\/api\/v1\/course\/class/
         ]
     },
     url: {
