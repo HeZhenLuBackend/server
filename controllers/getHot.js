799113188e839770ac2258e55b7ebc7d9d114f84
apple <875172749@qq.com>
2019-02-01 16:00:07 +0800

    add comment

--- controllers/getHot.js
+++ controllers/getHot.js
@@ -4,15 +4,8 @@ module.exports = async (ctx) =>{
     let result = await mysql('article').select('title',{id:'aid'},{coverPicture:'cover'},'script','type','date').orderBy('aid','desc').limit(4);
 
     for (let i=0; i<result.length; i++) {
-        console.log(result[i].coverPicture);
         result[i].coverPicture = eval(result[i].coverPicture)[0];
-        console.log(result[i].coverPicture);
     }
-    // for (let i=0; i<result.length; i++) {
-    //     result[i].coverPicture = result[i].coverPicture.substr(1,result[i].coverPicture.length - 2);
-    //     let array = result[i].coverPicture.split('","');
-    //     result[i].coverPicture = array[0];
-    // }
     ctx.body = result;
 
 };
