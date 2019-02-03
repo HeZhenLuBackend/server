8d1bef5e1f36831647f00cabe94c2fb22237aab5
apple <875172749@qq.com>
2019-02-03 20:42:55 +0800

    add comment

--- controllers/getHot.js
+++ controllers/getHot.js
@@ -1,7 +1,8 @@
 const mysql = require('../middleware/mysql');
 
 module.exports = async (ctx) =>{
-    let result = await mysql('article').select('title',{id:'aid'},{coverPicture:'cover'},'script','type','date').orderBy('aid','desc').limit(4);
+
+    let result = await mysql('article').join('articleread', 'article.aid', 'articleread.aid').select('title',{id:'aid'},{coverPicture:'cover'},'script','type','date').orderBy('readcount','desc').limit(4);
 
     for (let i=0; i<result.length; i++) {
         result[i].coverPicture = eval(result[i].coverPicture)[0];
