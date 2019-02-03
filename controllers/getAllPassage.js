fe65dd6514b7e74bb44699a686c253b1fb0011ab
apple <875172749@qq.com>
2019-02-03 20:51:03 +0800

    add comment

--- controllers/getAllPassage.js
+++ controllers/getAllPassage.js
@@ -2,7 +2,7 @@ const mysql = require('../middleware/mysql');
 
 module.exports = async (ctx) =>{
 
-    let result = await mysql('article').select('title',{id:'aid'},{coverPicture:'cover'},'script','type','date').orderBy('aid','desc');
+    let result = await mysql('article').select('title',{id:'aid'},{coverPicture:'cover'},'shortcontent','type','date').orderBy('aid','desc');
     for (let i=0; i<result.length; i++) {
         result[i].coverPicture = eval(result[i].coverPicture)[0];
         result[i].date = dateParser(result[i].date);
