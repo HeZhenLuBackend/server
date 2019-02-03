bea0e7c835456da5334a7ba8c517a967d79584b5
apple <875172749@qq.com>
2019-02-03 20:46:49 +0800

    add comment

--- controllers/getHot.js
+++ controllers/getHot.js
@@ -12,3 +12,19 @@ module.exports = async (ctx) =>{
     ctx.body = result;
 
 };
+
+
+function dateParser(timestamp) {
+    var interval = ( Date.parse(new Date()) - Date.parse(timestamp) ) / 1000;
+
+    if (interval < 3600) {
+        return '刚刚';
+    }
+    if ((interval/3600) < 24) {
+        return parseInt(interval/3600) + '小时前';
+    }
+    if ((interval/86400) < 8) {
+        return parseInt(interval/86400) + '天前';
+    }
+    return timestamp.substr(5, 5);
+}
--- controllers/getLatestComment.js
+++ controllers/getLatestComment.js
@@ -1,5 +1,25 @@
 const mysql = require('../middleware/mysql');
 
 module.exports = async (ctx) =>{
-    ctx.body = await mysql('comment').select({id:'cid'},{comment:'content'},{postTime:'create_time'},{passageID:'aid'}).orderBy('cid','desc').limit(2);
+    let result = await mysql('comment').select({id:'cid'},{comment:'content'},{postTime:'create_time'},{passageID:'aid'}).orderBy('cid','desc').limit(2);
+    for (let i=0; i<result.length; i++) {
+        result[i].postTime = dateParser(result[i].postTime);
+    }
+    ctx.body = result;
 };
+
+
+function dateParser(timestamp) {
+    var interval = ( Date.parse(new Date()) - Date.parse(timestamp) ) / 1000;
+
+    if (interval < 3600) {
+        return '刚刚';
+    }
+    if ((interval/3600) < 24) {
+        return parseInt(interval/3600) + '小时前';
+    }
+    if ((interval/86400) < 8) {
+        return parseInt(interval/86400) + '天前';
+    }
+    return timestamp.substr(5, 5);
+}
